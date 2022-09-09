64d0aa41c6d5336ebe5ad5e7b120c3aef2db01bd
revido <revidoberliano@gmail.com>
2022-09-09 09:07:32 +0700

    commit all the things

---
+++ README.md
@@ -0,0 +1,6 @@
+# infinity_scrool
+
+## OutPut
+![Screen Gesture 1](images/01.gif)
+
+Menggunakan listview dan english word package untuk  mengghasilkan random word
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:english_words/english_words.dart'; // Add this line.
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   // final wordPair = WordPair.random(); // Add this line.
+  //   return MaterialApp(
+  //     title: 'Welcome to Flutter',
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: const Text('Welcome to Flutter'),
+  //       ),
+  //       body: Center(
+  //         // Drop the const, and
+  //         //child: Text('Hello World'),        // Replace this text...
+  //         // child: Text(wordPair.asPascalCase),  // With this text.
+  //         child: RandomWords(),
+  //       ),
+  //     ),
+  //   );
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // NEW
+  final _biggerFont = const TextStyle(fontSize: 18); // NEW
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   final wordPair = WordPair.random(); // NEW
+  //   return Text(wordPair.asPascalCase); // NEW
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
